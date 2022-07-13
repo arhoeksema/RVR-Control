fd195a3827c4a44833f72d31da0afaabe7258e6d
arhoeksema <109236668+arhoeksema@users.noreply.github.com>
2022-07-13 12:05:57 -0500

    Add files via upload

---
+++ (test) LGPIO & PWM.py
@@ -0,0 +1,15 @@
+import lgpio
+import time
+
+in1_pin = 13
+frequency = 5000
+h = lgpio.gpiochip_open(0)
+
+try:
+    while True:
+        lgpio.tx_pwm(h, in1_pin, frequency, 63.168)
+        time.sleep(.02)
+
+except KeyboardInterrupt:
+    lgpio.tx_pwm(h, in1_pin, frequency, 0)
+    lgpio.gpiochip_close(h)
